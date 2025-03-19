b22476287c984b4bc8627d82d51bd0fef5699e69
Sloane M Velazco <sloanemvelazco@Sloanes-Air.lw.hs>
2025-03-19 09:59:14 -0700

    header

---
+++ src/assets/Header.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+const Logo = () => {
+    return(
+        <h1 className="black-text"><span className="rose-text">First</span> and <span className="mustard-text">Proud</span></h1>
+    );
+}
+
+// update links in navbar once you have finished other parts
+
+const Navbar = () => {
+    return (
+        <nav className="rose-background">
+            <ul>
+                <li><NavLink to="/" className="black-text small-font">Home</NavLink></li>
+                <li><NavLink to="/" className="black-text small-font">College Match</NavLink></li>
+                <li><NavLink to="/" className="black-text small-font">Contact</NavLink></li>
+            </ul>
+        </nav>
+    );
+}
+
+function Header() {
+    return(
+        <header>
+            <Logo />
+            <Navbar />
+        </header>
+    );
+}
+
+export default Header;
