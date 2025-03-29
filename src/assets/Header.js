af59e017f5007d58ea823a98710588083f5dace3
Sloane M Velazco <sloanemvelazco@Sloanes-Air.hsd1.ca.comcast.net>
2025-03-28 20:05:13 -0700

    finished homepage

--- src/assets/Header.js
+++ src/assets/Header.js
@@ -9,12 +9,20 @@ const Logo = () => {
 // update links in navbar once you have finished other parts
 
 const Navbar = () => {
+
+    const toContact = () => {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight, // scroll to the bottom of home page
+            behavior: 'smooth'
+        });
+    };
+
     return (
-        <nav className="flex-parent istok-web">
+        <nav className="flex-parent istok-web" id="navbar">
             <ul>
-                <li><NavLink to="/" className="black-text small-font">Home</NavLink></li>
-                <li><NavLink to="/" className="black-text small-font">College Match</NavLink></li>
-                <li><NavLink to="/" className="black-text small-font">Contact</NavLink></li>
+                <li><NavLink className="black-text small-font">Home</NavLink></li>
+                <li><NavLink className="black-text small-font">College Match</NavLink></li>
+                <li><NavLink onClick={toContact} className="black-text small-font">Contact</NavLink></li>
             </ul>
         </nav>
     );
--- src/assets/Homepage.js
+++ src/assets/Homepage.js
@@ -1,17 +1,17 @@
 function Lander() {
   return (
-      <article className="flex-parent">
+      <article className="flex-parent" id="lander-section">
           <section className="med-padding">
-              <p className="black-text bree-serif xlarge-font">
-                  Attention<br />
-                  <span className="mustard-text">Grabber</span>
-                  <br />Here
-              </p>
+              <h2 className="bree-serif black-text xlarge-font">
+                  Supporting<br />
+                  <span className="mustard-text">First Gen</span>
+                  <br />Students
+              </h2>
               <div className="line rose-bg"></div>
               <button className="inria-sans small-font">LEARN MORE</button>
           </section>
           <figure>
-              <img src="/images/placeholder1.png" alt="Placeholder Image" className="lander-img" />
+              <img src="/images/gradcap.png" id="lander-image" alt="Graduation cap" className="lander-img" width="100%" />
           </figure>
       </article>
   );
@@ -19,12 +19,39 @@ function Lander() {
 
 function Information() {
   return (
-      <article className="flex-parent">
-          <figure className="med-padding">
-              <img src="/images/images.png" alt="Information Image" />
+      <article className="flex-parent" id="info-section">
+        <div className="bg-square peach-bg" style={{ top: "950px" }}></div>
+          <figure>
+              <img className="small-img" src="/images/members.jpg" alt="Members of First and Proud club" width="100%" height="500"/>
           </figure>
+          <section>
+            <h3 className="bree-serif yellow-text large-font">ABOUT US</h3>
+            <p className="inria-sans black-text small-font" id="information-text">We are a student-led group dedicated to uplifting and supporting first-generation college students throughout their high school journey and college application process. We host panels, workshops, and information sessions at Lick-Wilmerding High School for first-gen students and collaborate with groups like First Graduate to provide resources and guidance to students beyond Lick-Wilmerding.</p>
+          </section>
       </article>
   );
 }
 
-export { Information, Lander };
+function Contact() {
+    return(
+        <article className="flex-parent yellow-bg" id="contact-section">
+            <section>
+                <h3 className="bree-serif rose-text large-font">GET IN TOUCH</h3>
+            </section>
+            <section>
+                <ul className="inria-sans mustard-text large-font">
+                    <li className="flex-parent contact-item">
+                        <img src="/images/icons/email.png" alt="Gmail logo" width="80" height="60"/>
+                        <p>PLACEHOLDER@GMAIL.COM</p>
+                    </li>
+                    <li className="flex-parent contact-item">
+                        <img src="/images/icons/insta.png" alt="Instagram logo" width="80" height="80"/>
+                        <p>@LWHSFIRSTANDPROUD</p>
+                    </li>
+                </ul>
+            </section>
+        </article>
+    );
+}
+
+export { Information, Lander,  Contact };
