3a31cdf640b77f4096397bb70272bad84e58342c
Sloane M Velazco <sloanemvelazco@Sloanes-Air.hsd1.ca.comcast.net>
2025-04-03 17:48:48 -0700

    Resolved merge conflict in package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles.css";
@@ -16,29 +15,6 @@ function App() {
       <Information />
       <Contact />
     </BrowserRouter>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 7fcfdaf (first commit)
   );
 }
 
