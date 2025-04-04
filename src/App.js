dea007a5bf68ca4900873b2ce75da335162ebdf1
Sloane M Velazco <sloanemvelazco@Sloanes-Air.hsd1.ca.comcast.net>
2025-04-03 17:44:36 -0700

    rebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles.css";
@@ -15,6 +16,29 @@ function App() {
       <Information />
       <Contact />
     </BrowserRouter>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> 7fcfdaf (first commit)
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import "./styles.css";
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> 7fcfdaf (first commit)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +18,10 @@ root.render(
   </React.StrictMode>
 );
 
+<<<<<<< HEAD
+=======
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+>>>>>>> 7fcfdaf (first commit)
 reportWebVitals();
